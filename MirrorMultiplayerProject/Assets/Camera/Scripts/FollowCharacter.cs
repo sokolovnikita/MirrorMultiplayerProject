c74adbc4a7839232a2662b9e10ba7e554661b7f1
Sokolov Nikita <sokolovnikita1240@gmail.com>
2023-05-06 22:53:16 +0300

    feat(camera-follow-character): add camera follow character

---
+++ MirrorMultiplayerProject/Assets/Camera/Scripts/FollowCharacter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour
+{
+    [SerializeField] private CharacterBase _character;
+    [SerializeField] private float _xOffset;
+    [SerializeField] private float _yOffset;
+    [SerializeField] private float _zOffset;
+    [SerializeField] private float _xRotationOffset;
+    [SerializeField] private float _yRotationOffset;
+    [SerializeField] private float _zRotationOffset;
+
+    private void Update()
+    {
+        Follow();       
+    }
+
+    private void Follow()
+    {
+        Vector3 positionOffset = new Vector3(_xOffset, _yOffset, _zOffset);
+        transform.position = _character.transform.TransformPoint(positionOffset);
+
+        Vector3 rotationOffset = new Vector3(_xRotationOffset, _character.transform.eulerAngles.y 
+            + _yRotationOffset, _zRotationOffset);
+        transform.rotation = Quaternion.Euler(rotationOffset);
+    }
+}
